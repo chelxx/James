f3066085406b89c6a77ac3bbb8272fe1169545d1
Chelsea Cabalan <chex239@gmail.com>
2018-05-31 13:33:50 -0500

    Hello James

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// FAQ COORDION
+$(function () {
+    $("#faq-questions").accordion({
+        header: "h1",
+        collapsible: true,
+        active: false,
+        autoHeight: false,
+        heightStyle: "content"
+    });
+});
+
+// INFO TABS
+$(function () {
+    $("#tabs").tabs();
+});
+
+// GALLERY TABS
+$(function () {
+    $("#gallery").tabs();
+});
